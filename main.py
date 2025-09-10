b3fbe18b1b4385d748f6db44557be844be77e041
alex-abdullah <alexradja97@gmail.com>
2025-09-11 09:48:12 +1000

    adding asteroid game

---
+++ main.py
@@ -0,0 +1,59 @@
+import sys
+import pygame
+from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = asteroids, updatable, drawable
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = updatable, drawable
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    shots = pygame.sprite.Group()
+    Shot.containers = updatable, drawable, shots
+
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        updatable.update(dt)
+
+        for obj in asteroids:
+            if obj.collision_check(player):
+                print("Game over!")
+                sys.exit()
+
+            for bullet in shots:
+                if obj.collision_check(bullet):
+                    bullet.kill()                
+                    obj.split()
+        
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
+if __name__ == "__main__":
+    main()
